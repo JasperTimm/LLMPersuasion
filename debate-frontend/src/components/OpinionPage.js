7a143f9f8723f049b693aa9cb2111c1d4086ff6f
Jasper Timm <jaspa@jaspa.codes>
2024-09-18 13:50:21 -0400

    Attempt at adding flow diagrams

--- debate-frontend/src/components/OpinionPage.js
+++ debate-frontend/src/components/OpinionPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../styles/OpinionPage.css';
+import IntialOpinionDiagram from '../assets/images/initial_opinion.png';
 
 const OpinionPage = ({ debate, setDebate }) => {
     const [opinion, setOpinionText] = useState('');
@@ -9,21 +10,24 @@ const OpinionPage = ({ debate, setDebate }) => {
     };
 
     return (
-      <div className='container'>
-        <h2 className="debate-title">Initial Opinion</h2>
-        <h3>
-          Consider the debate topic above and share your initial opinion.
-          Just one or two sentences to summarize your stance.
-        </h3>
-        <textarea
-          className="input-textarea"
-          value={opinion}
-          onChange={(e) => setOpinionText(e.target.value)}
-          placeholder="Briefly summarize your opinion on this topic in 1-2 sentences"
-        />
-        <button className="submit-button" onClick={submitOpinion}>
-          Submit Opinion
-        </button>
+      <div>
+        <div className='container'>
+          <h2 className="debate-title">Initial Opinion</h2>
+          <h3>
+            Consider the debate topic above and share your initial opinion.
+            Just one or two sentences to summarize your stance.
+          </h3>
+          <textarea
+            className="input-textarea"
+            value={opinion}
+            onChange={(e) => setOpinionText(e.target.value)}
+            placeholder="Briefly summarize your opinion on this topic in 1-2 sentences"
+          />
+          <button className="submit-button" onClick={submitOpinion}>
+            Submit Opinion
+          </button>
+        </div>
+        <img src={IntialOpinionDiagram} alt="Initial Opinion Diagram" className="initial-opinion-diagram" />
       </div>
     );
 };
